0cc1c6837c69c1f54f4eabee121085b1ddf02c9c
psandstrom14 <psandstrom8@gmail.com>
2026-01-12 17:02:30 -0700

    Add project files.

---
+++ DiceRollerClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission_2_Assignment
+{
+    internal class DiceRollerClass
+    {
+        public int[] RollDice(int numRolls)
+        {
+            Random rand = new Random();
+            int[] results = new int[13];
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = rand.Next(1, 7);
+                int die2 = rand.Next(1, 7);
+                int result = die1 + die2;
+                results[result]++;
+            }
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Mission_2_Assignment {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("No Time To Dice");
+            Console.WriteLine("How many times would you like to roll the dice?");
+            int numRolls = int.Parse(Console.ReadLine());
+            DiceRollerClass diceRoller = new DiceRollerClass();
+            int[] results = diceRoller.RollDice(numRolls);
+
+            for (int i = 2; i < results.Length; i++)
+            {
+                double resultPercentNum = results[i] / (double)numRolls * 100;
+                int roundedResultPercentNum = (int)Math.Round(resultPercentNum);
+                string asterisks = "";
+                for (int j = 0; j < roundedResultPercentNum; j++)
+                {
+                    asterisks += "*";
+                }
+                Console.WriteLine($"{i}: {asterisks}");
+            }
+        }
+
+    }
+}
